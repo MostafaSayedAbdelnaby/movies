86f020032f8722f26605ec844954b06f8ff0cef8
osamaebaid1973 <115580703+osamaebaid1973@users.noreply.github.com>
2025-02-06 12:31:00 +0200

    Add files via upload

---
+++ lib/core/widgets/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color backgroundColor = Color(0XFF121312);
+  static const  Color primaryColor = Color(0XFFF6BD00);
+  static const textColor = Color(0XFFFFFFFF);
+  static const redColor = Color(0XFFE82626);
+}
