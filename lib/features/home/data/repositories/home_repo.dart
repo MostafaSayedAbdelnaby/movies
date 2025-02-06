c1f7261dbb2777f11c9f4f815088e425b8d290bd
osamaebaid1973 <115580703+osamaebaid1973@users.noreply.github.com>
2025-02-06 12:40:34 +0300

    Update home_repo.dart

--- lib/features/home/data/repositories/home_repo.dart
+++ lib/features/home/data/repositories/home_repo.dart
@@ -3,8 +3,8 @@ import 'package:movies_app/features/home/domain/entities/movie_entity.dart';
 
 class HomeRepo{
  ApiHelper apiHelper = ApiHelper();
-  Future<List<MovieEntity>> getMovies() async {
-   var data = await apiHelper.getMovies();
+  Future<List<MovieEntity>> getWatchNowMovies() async {
+   var data = await apiHelper.getWatchNowMovies();
    var movieEntityList = data.map((item) => item as MovieEntity).toList();
    return movieEntityList;
   }
