a0a0900aea476666592ce78c87de1d40f07eda90
osamaebaid1973 <115580703+osamaebaid1973@users.noreply.github.com>
2025-02-11 01:27:03 +0200

    movie details changes

--- lib/screen/movie_details_screen.dart
+++ lib/screen/movie_details_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:movies_app/api_helper/api_helper.dart';
+import 'package:movies_app/core/widgets/app_colors.dart';
+import 'package:movies_app/core/widgets/movies_elevated_button.dart';
 
 class MovieDetailsScreen extends StatelessWidget {
   static const routeName = '/movie-details-screen';
@@ -20,10 +22,10 @@ class MovieDetailsScreen extends StatelessWidget {
 
           return Scaffold(
             backgroundColor: Colors.transparent,
-              appBar: AppBar(
-                backgroundColor: Colors.transparent,
-              ),
-            body: Stack(alignment: Alignment.bottomCenter,
+            appBar: AppBar(
+              backgroundColor: Colors.transparent,
+            ),
+            body: Stack(
               children: [
                 Container(
                   decoration: BoxDecoration(
@@ -35,11 +37,96 @@ class MovieDetailsScreen extends StatelessWidget {
                   ),
                   child: null /* add child content here */,
                 ),
-                Text(
-                  snapshot.data!.title,style: const TextStyle(color: Colors.white,fontSize: 24),)
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                    
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [const Spacer(),
+                      Text(
+                        snapshot.data!.title,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 16),
+                      Text(
+                        '${snapshot.data!.releaseDate}',
+                        style: const TextStyle(
+                            color: Colors.white, fontSize: 24),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 16),
+                      MoviesElevatedButton(
+                          backgroundColor: AppColors.redColor,
+                          child: const Text('Watch',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 20)),
+                          onPressed: () {}),
+                      const SizedBox(height: 16),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          RowItem(
+                              imageName: 'vote_count',
+                              data: snapshot.data!.voteCount),
+                          const SizedBox(width: 16),
+                          RowItem(
+                              imageName: 'run_time',
+                              data: snapshot.data!.runtime),
+                          const SizedBox(width: 16),
+                          RowItem(
+                              imageName: 'vote_average',
+                              data: snapshot.data!.voteAverage),
+                        ],
+                      ),
+                      const SizedBox(height: 16),
+                      const Text(
+                        'Screen Shots',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                        ),
+                        
+                      ),
+                      const SizedBox(height: 16),
+                      
+                    ],
+                  ),
+                ),
               ],
             ),
           );
         });
+  }
+}
+
+class RowItem extends StatelessWidget {
+  String imageName;
+  num data;
+  RowItem({
+    required this.imageName,
+    required this.data,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 47,
+      width: 100,
+      decoration: BoxDecoration(
+          color: AppColors.backgroundColor,
+          borderRadius: BorderRadius.circular(16)),
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+        Image.asset('assets/images/$imageName.png'),
+        Text(
+          '$data',
+          style: const TextStyle(color: Colors.white, fontSize: 20),
+        )
+      ]),
+    );
   }
 }
