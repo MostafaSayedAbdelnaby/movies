c8235676dc4b286abd7a37bcf263c2a5800cbd89
Mostafa Sayed <mostafaalawamy9@gmail.com>
2025-02-20 01:49:55 +0200

    register screen & create user

--- lib/features/auth/register/presentation/screens/register_screen.dart
+++ lib/features/auth/register/presentation/screens/register_screen.dart
@@ -39,7 +39,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
       create: (context) {
         return RegisterCubit(registerRepo: RegisterRepoImpl(DataSourceImpl()));
       },
-      child: BlocListener<RegisterCubit, RegisterStates>(
+      child: BlocConsumer<RegisterCubit, RegisterStates>(
         listener: (BuildContext context, state) {
           if (state is RegisterOnSuccessState) {
             Navigator.pushNamedAndRemoveUntil(
@@ -68,154 +68,155 @@ class _RegisterScreenState extends State<RegisterScreen> {
             );
           }
         },
-        child: Scaffold(
-          resizeToAvoidBottomInset: false,
-          appBar: AppBar(
-            title: const Text('Register'),
-          ),
-          body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: SingleChildScrollView(
-              child: Form(
-                key: formKey,
-                child: Column(children: [
-                  const AvatarCarouselWidget(),
-                  Text("Avatar",
-                      textAlign: TextAlign.center,
-                      style: textTheme.headlineSmall),
-                  const SizedBox(height: 10),
-                  MoviesTextFormField(
-                    textEditingController: nameController,
-                    prefixIconImageName: 'name',
-                    labelText: 'Name',
-                    onTap: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Name is Required";
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 16),
-                  MoviesTextFormField(
-                    textEditingController: emailController,
-                    prefixIconImageName: 'email',
-                    labelText: 'Email',
-                    onTap: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Email is Required";
-                      }
-                      final bool emailValid =
-                          RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9]+@[gmail]+\.[com]+")
-                              .hasMatch(value);
-                      if (!emailValid) {
-                        return "Email is Required with RegExp";
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 16),
-                  MoviesTextFormField(
-                    textEditingController: passwordController,
-                    prefixIconImageName: 'password',
-                    labelText: 'password',
-                    suffixIconImageName: 'show_password',
-                    onTap: (value) {
-                      if (value == null || value.isEmpty) {
-                        return "Password is Required";
-                      }
-                      if (value.length < 6) {
-                        return "Password should be at least 6 Char ";
-                      }
-                      if (passwordController.text != value) {
-                        return "Password not matched ";
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 16),
-                  MoviesTextFormField(
-                    textEditingController: rePasswordController,
-                    prefixIconImageName: 'password',
-                    labelText: 'Confirm Password',
-                    suffixIconImageName: 'show_password',
-                    onTap: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Password is Required';
-                      }
-                      if (value.length < 6) {
-                        return 'Password should be at least 6 Char';
-                      }
-                      if (passwordController.text != value) {
-                        return 'Password not matched';
-                      }
-                      return null;
-                    },
-                  ),
-                  const SizedBox(height: 16),
-                  MoviesTextFormField(
-                    textEditingController: phoneNumberController,
-                    onTap: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'enter_phone_number';
-                      }
-                      final phoneRegex =
-                          RegExp(r'^(?:\+20|0)?1[0-2,5]{1}[0-9]{8}$');
-                      if (!phoneRegex.hasMatch(value)) {
-                        return 'please_enter_a_valid_phone_number';
-                      }
-                      return null;
-                    },
-                    prefixIconImageName: 'phone',
-                    labelText: 'Phone',
-                  ),
-                  const SizedBox(height: 16),
-                  MoviesElevatedButton(
-                    color: AppColors.primaryColor,
-                    child: Text(
-                      'Create Account',
-                      style: textTheme.labelLarge!
-                          .copyWith(color: AppColors.backgroundColor),
+        builder: (context, state) {
+          return Scaffold(
+            resizeToAvoidBottomInset: false,
+            appBar: AppBar(
+              title: const Text('Register'),
+            ),
+            body: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: SingleChildScrollView(
+                child: Form(
+                  key: formKey,
+                  child: Column(children: [
+                    const AvatarCarouselWidget(),
+                    Text("Avatar",
+                        textAlign: TextAlign.center,
+                        style: textTheme.headlineSmall),
+                    const SizedBox(height: 10),
+                    MoviesTextFormField(
+                      textEditingController: nameController,
+                      prefixIconImageName: 'name',
+                      labelText: 'Name',
+                      onTap: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Name is Required";
+                        }
+                        return null;
+                      },
                     ),
-                    onPressed: () {
-                      // currentState means in the click state
-                      if (formKey.currentState!.validate()) {
-                        // context.read<RegisterCubit>().addUser(
-                        //     UserModel(
-                        //         name: nameController.text,
-                        //         id: "",
-                        //         email: emailController.text,
-                        //         indexOfImage: indexOfImage,
-                        //         password: passwordController.text,
-                        //         phoneNumber: phoneNumberController.text));
-                      }
-                    },
-                  ),
-                  const SizedBox(height: 14),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Already have an account ? ',
-                        style: textTheme.labelMedium?.copyWith(
-                            fontSize: 14, color: AppColors.textColor),
+                    const SizedBox(height: 16),
+                    MoviesTextFormField(
+                      textEditingController: emailController,
+                      prefixIconImageName: 'email',
+                      labelText: 'Email',
+                      onTap: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Email is Required";
+                        }
+                        final bool emailValid =
+                        RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9]+@[gmail]+\.[com]+")
+                            .hasMatch(value);
+                        if (!emailValid) {
+                          return "Email is Required with RegExp";
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16),
+                    MoviesTextFormField(
+                      textEditingController: passwordController,
+                      prefixIconImageName: 'password',
+                      labelText: 'password',
+                      suffixIconImageName: 'show_password',
+                      onTap: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Password is Required";
+                        }
+                        if (value.length < 6) {
+                          return "Password should be at least 6 Char ";
+                        }
+                        if (passwordController.text != value) {
+                          return "Password not matched ";
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16),
+                    MoviesTextFormField(
+                      textEditingController: rePasswordController,
+                      prefixIconImageName: 'password',
+                      labelText: 'Confirm Password',
+                      suffixIconImageName: 'show_password',
+                      onTap: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Password is Required';
+                        }
+                        if (value.length < 6) {
+                          return 'Password should be at least 6 Char';
+                        }
+                        if (passwordController.text != value) {
+                          return 'Password not matched';
+                        }
+                        return null;
+                      },
+                    ),
+                    const SizedBox(height: 16),
+                    MoviesTextFormField(
+                      textEditingController: phoneNumberController,
+                      onTap: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'enter_phone_number';
+                        }
+                        final phoneRegex =
+                        RegExp(r'^(?:\+20|0)?1[0-2,5]{1}[0-9]{8}$');
+                        if (!phoneRegex.hasMatch(value)) {
+                          return 'please_enter_a_valid_phone_number';
+                        }
+                        return null;
+                      },
+                      prefixIconImageName: 'phone',
+                      labelText: 'Phone',
+                    ),
+                    const SizedBox(height: 16),
+                    MoviesElevatedButton(
+                      color: AppColors.primaryColor,
+                      child: Text(
+                        'Create Account',
+                        style: textTheme.labelLarge!
+                            .copyWith(color: AppColors.backgroundColor),
                       ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamed(context, AppRoutes.loginRoute);
-                        },
-                        child: Text(
-                          'Login',
-                          style: textTheme.labelMedium?.copyWith(fontSize: 14),
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          context.read<RegisterCubit>().addUser(
+                              UserModel(
+                                  name: nameController.text,
+                                  id: "",
+                                  email: emailController.text,
+                                  indexOfImage: 0,
+                                  password: passwordController.text,
+                                  phoneNumber: phoneNumberController.text));
+                        }
+                      },
+                    ),
+                    const SizedBox(height: 14),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Already have an account ? ',
+                          style: textTheme.labelMedium?.copyWith(
+                              fontSize: 14, color: AppColors.textColor),
                         ),
-                      )
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                ]),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context, AppRoutes.loginRoute);
+                          },
+                          child: Text(
+                            'Login',
+                            style: textTheme.labelMedium?.copyWith(fontSize: 14),
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                  ]),
+                ),
               ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
